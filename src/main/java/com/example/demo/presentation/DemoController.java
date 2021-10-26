c5d02793f3bd855f975afa498b7cb9515a2d5752
Shukupon <67812013+Shukupon@users.noreply.github.com>
2021-10-26 23:14:36 +0900

    logのtest追加

--- src/main/java/com/example/demo/presentation/DemoController.java
+++ src/main/java/com/example/demo/presentation/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo.presentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,14 +20,19 @@ import com.example.demo.bean.Goods;
 @RequestMapping("/demo")
 public class DemoController {
 
+	private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
 	@Autowired
 	DemoService demoService;
 
 	@PostMapping("/shopping")
 	public ResponseEntity<Boolean> shopping(@RequestBody Goods goods) {
 
+		logger.info("処理開始");
 		Boolean response = demoService.decide(goods);
+		logger.info("処理完了");
 
+		
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
 		return ResponseEntity.status(HttpStatus.OK).headers(responseHeaders).body(response);
--- src/test/java/com/example/demo/presentation/DemoControllerTest.java
+++ src/test/java/com/example/demo/presentation/DemoControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.presentation;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -9,9 +10,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,8 +25,34 @@ import com.example.demo.application.service.DemoService;
 import com.example.demo.bean.Goods;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+
 @WebMvcTest(DemoController.class)
+@ExtendWith(OutputCaptureExtension.class)
 public class DemoControllerTest {
+
+	//Appenderのモック.
+	private ListAppender<ILoggingEvent> listAppender;
+
+	//キャプチャー.
+	@Captor
+	private ArgumentCaptor<ILoggingEvent> captor;
+
+/**
+  * 事前設定
+  */
+//@BeforeEach
+//public void setUp() {
+// 
+//    Logger logger = (Logger)LoggerFactory.getLogger(DemoController.class);
+//    listAppender = new ListAppender<>();
+//    listAppender.start();
+//    logger.addAppender(listAppender);
+// 
+//    captor = ArgumentCaptor.forClass(ILoggingEvent.class);
+// 
+//}
 
 	@Autowired
 	MockMvc mockMvc;
@@ -29,7 +61,7 @@ public class DemoControllerTest {
 	DemoService mockService;
 
 	@Test
-	public void shoppingTest01() throws Exception {
+	public void shoppingTest01(CapturedOutput output) throws Exception {
 
 		// 使用する商品情報の準備
 		Goods goods = createGoods();
@@ -49,6 +81,9 @@ public class DemoControllerTest {
 		Boolean result = objectMapper.readValue(responseJson, Boolean.class);
 		verify(mockService, times(1)).decide(any(Goods.class));
 		assertTrue(result);
+		assertThat(output).contains("処理開始");
+		assertThat(output).contains("処理完了");
+
 	}
 
 	private Goods createGoods() {
